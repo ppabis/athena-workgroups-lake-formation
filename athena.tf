2248e2a2a051e3b5e15a9ea7011c380f6081079b
ppabis <piotr@pabis.eu>
2024-01-05 21:01:09 +0100

    Defined Athena workgroup with limits

--- athena.tf
+++ athena.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "athena" {
   bucket = var.athena_results_bucket
 }
+
+resource "aws_athena_workgroup" "limited-wg" {
+  name = "limited-wg"
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+    bytes_scanned_cutoff_per_query     = 1024 * 1024 * 20
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena.id}/limited-wg/"
+    }
+  }
+}
