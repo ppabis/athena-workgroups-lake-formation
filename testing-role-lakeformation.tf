0f3ed16761ba190ba4b9ff5ce36f5850708023d6
ppabis <piotr@pabis.eu>
2024-01-08 20:54:20 +0100

    Added lakeformation permissions

---
+++ testing-role-lakeformation.tf
@@ -0,0 +1,29 @@
+data "aws_glue_catalog_table" "cloudfront_logs" {
+  database_name = "default"
+  name          = "cloudfront_logs"
+}
+
+data "aws_iam_policy_document" "LakeFormationGlueTablesAccess" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "lakeformation:GetDataAccess",
+      "lakeformation:GetResourceLFTags",
+      "lakeformation:ListLFTags",
+      "lakeformation:GetLFTag",
+      "lakeformation:SearchDatabasesByLFTags"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "LakeFormationGlueTablesAccess" {
+  name        = "LakeFormationGlueTablesAccess"
+  description = "Lakeformation access to cloudfront_logs table"
+  policy      = data.aws_iam_policy_document.LakeFormationGlueTablesAccess.json
+}
+
+resource "aws_iam_role_policy_attachment" "LakeFormationGlueTablesAccess" {
+  policy_arn = aws_iam_policy.LakeFormationGlueTablesAccess.arn
+  role       = aws_iam_role.AthenaWorkgroupUserRole.name
+}
